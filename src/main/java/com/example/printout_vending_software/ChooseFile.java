9f2e4f2573c293fd5dc196de101cb92dd401544e
jordansoans2002 <89215008+jordansoans2002@users.noreply.github.com>
2022-10-27 11:01:06 +0530

    Add files via upload

--- src/main/java/com/example/printout_vending_software/ChooseFile.java
+++ src/main/java/com/example/printout_vending_software/ChooseFile.java
@@ -1,32 +1,35 @@
 package com.example.printout_vending_software;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
+
 import javax.print.*;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.standard.*;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.io.*;
 
 public class ChooseFile {
     public static void main(String []args){
         new ChooseFile().printSelectedFile();
     }
 
-    void printSelectedFile(){
+    void printSelectedFile() {
         try {
-            String fileDirectory = "C:\\Users\\Hanniel\\Desktop\\Jordan\\Printout vending software\\src\\main\\resources\\com\\example\\printout_vending_software\\test files";
+            String fileDirectory = "C:\\Users\\chris\\Downloads\\Printout_Vending_Machine-learn-printing\\Printout_Vending_Machine-learn-printing\\src\\main\\resources\\com\\example\\printout_vending_software\\test files";
             File startingDirectory = new File(fileDirectory);
             JFileChooser selectFile = new JFileChooser(fileDirectory);//"H:\\");
             FileNameExtensionFilter filterSupported = new FileNameExtensionFilter("supported types", "jpg", "png", "pdf", "doc", "docx");
             FileNameExtensionFilter filterImages = new FileNameExtensionFilter("images", "jpg", "png", "jfif");
             FileNameExtensionFilter filterDocs = new FileNameExtensionFilter("documents", "pdf", "doc", "docx");
             String supported = "jpg png pdf doc docx JPG PNG PDF DOC DOCX";
+            String ext="";
             InputStream selectedFile = null;
-
+            PDDocument pdfDoc = null;
             while (true) {
                 selectFile.setCurrentDirectory(startingDirectory);
                 selectFile.setFileFilter(filterSupported);
@@ -36,33 +39,48 @@ public class ChooseFile {
                 if (selection == JFileChooser.APPROVE_OPTION) {
                     File selected = selectFile.getSelectedFile();
                     String path = selected.getAbsolutePath();
-                    String ext = path.substring(path.lastIndexOf(".")+1);
+                    ext = path.substring(path.lastIndexOf(".")+1);
                     if (path.startsWith(fileDirectory) && supported.contains(ext)) {
                         selectedFile = new FileInputStream(path);
+                        if(ext.equalsIgnoreCase("pdf"))
+                            pdfDoc=PDDocument.load(selectedFile);
                         break;
                     }
                 } else if (selection == JFileChooser.CANCEL_OPTION)
                     break;
             }
-            DocFlavor docFlavorSelected = DocFlavor.INPUT_STREAM.JPEG;
-            Doc docSelected = new SimpleDoc(selectedFile, docFlavorSelected, null);
 
             PrintRequestAttributeSet aset=new HashPrintRequestAttributeSet();
             aset.add(MediaSizeName.ISO_A10);
             aset.add(Chromaticity.MONOCHROME);
-            aset.add(OrientationRequested.LANDSCAPE);
-            aset.add(new Copies(2));
-            aset.add(new JobPriority(5));
+            //aset.add(OrientationRequested.LANDSCAPE);
+            aset.add(new Copies(1));
+            //aset.add(new JobPriority(5));
 
             PrintService[] services= PrintServiceLookup.lookupPrintServices(null,null);
-            DocPrintJob job= ServiceUI.printDialog(null,0,0,services,services[2],null,aset).createPrintJob();
+            DocPrintJob job= ServiceUI.printDialog(null,0,0,services,services[3],null,aset).createPrintJob();
 
-            try{
-                job.print(docSelected,aset);
-            }catch (PrintException e){
-                System.out.println("print exception "+ e.getMessage());
+            if(!ext.equalsIgnoreCase("pdf")){
+                DocFlavor docFlavorSelected = DocFlavor.INPUT_STREAM.JPEG;
+                Doc docSelected = new SimpleDoc(selectedFile, docFlavorSelected, null);
+                try{
+                    job.print(docSelected,aset);
+                }catch (PrintException e){
+                    System.out.println("print exception "+ e.getMessage());
+                }
             }
-        }catch(FileNotFoundException e){
+            else{
+                PrinterJob pdfPrintJob = PrinterJob.getPrinterJob();
+                pdfPrintJob.setPageable(new PDFPageable(pdfDoc));
+                try {
+                    pdfPrintJob.setPrintService(job.getPrintService());
+                    pdfPrintJob.print();
+                } catch (PrinterException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch(IOException  e){
             e.printStackTrace();
         }
     }
--- src/main/java/com/example/printout_vending_software/PDFDocs.java
+++ src/main/java/com/example/printout_vending_software/PDFDocs.java
@@ -1,18 +1,41 @@
 package com.example.printout_vending_software;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.printing.PDFPageable;
+
+import javax.print.DocPrintJob;
+import javax.print.PrintService;
+import javax.print.PrintServiceLookup;
+import javax.print.ServiceUI;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.io.File;
 import java.io.IOException;
 
 public class PDFDocs {
     public static void main(String[] args){
-        new PDFDocs();
+        new PDFDocs().loadPDF();
     }
 
-    void loadPDF() throws IOException {
-        File pdfFile=new File("src/main/resources/com/example/printout_vending_software/test files/Proposal.pdf");
-        PDDocument pdfDoc=PDDocument.load(pdfFile);
+    void loadPDF() {
+        try {
+            File pdfFile=new File("C:\\Users\\chris\\Downloads\\Printout_Vending_Machine-learn-printing\\Printout_Vending_Machine-learn-printing\\src\\main\\resources\\com\\example\\printout_vending_software\\test files\\Proposal.pdf");
+            PDDocument pdfDoc=PDDocument.load(pdfFile);
+            PrintService[] services= PrintServiceLookup.lookupPrintServices(null,null);
+            //DocPrintJob job= ServiceUI.printDialog(null,0,0,services,services[2],null,null).createPrintJob();
+
+            for(int i=0;i<services.length;i++){
+                System.out.println(services[i].getName());
+            }
+            PrinterJob job = PrinterJob.getPrinterJob();
+            job.setPageable(new PDFPageable(pdfDoc));
+            job.setPrintService(services[3]);
+            job.print();
+            System.out.println("printed");
+        } catch (PrinterException | IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
